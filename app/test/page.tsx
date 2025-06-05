da81e13d0fe1facae31df9b6d2e8706de174fcf5
牧卓見 <takumi_maki@a.jsl.co.jp>
2025-06-05 17:23:46 +0900

    fetchをコメントアウト

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,13 +1,13 @@
 import RevalidateTest from "@/components/revalidate";
 
 export default async function Page() {
-  const fuga = await fetch("https://main.d1zjxntokfr2e6.amplifyapp.com/api", {
-    next: { tags: ["test-revalidate"] },
-  });
-  const resp = await fuga.json();
+  // const fuga = await fetch("https://main.d1zjxntokfr2e6.amplifyapp.com/api", {
+  //   next: { tags: ["test-revalidate"] },
+  // });
+  // const resp = await fuga.json();
   return (
     <>
-      <div>apiresponse: {JSON.stringify(resp)}</div>
+      {/* <div>apiresponse: {JSON.stringify(resp)}</div> */}
       <div>
         <RevalidateTest />
       </div>
